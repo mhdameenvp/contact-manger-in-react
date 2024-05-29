ed2f8ff3aa408d0368ef70855cfb2f4150c1544b
mhdameenvp <mhdameenvp1126@gmail.com>
2024-05-29 06:32:58 -0700

    first commit

---
+++ src/componets/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./css/Headers.css"
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container">
+        <h2 className="contact-manger">Contact Manager</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
